0edbbb258501063f4931eda290b6ded91102e896
VladimirDrevin <vladimir.drevin.1@iliauni.edu.ge>
2026-04-10 18:08:34 +0400

    Initial setup test

---
+++ test-backend/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+// 🔌 MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// 🧪 test route
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
